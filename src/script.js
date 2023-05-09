1a1db79ecca849f22fb9965ca1089f89759671ae
Folarin Lawal <lawalfolarin6@gmail.com>
2023-05-09 16:17:43 +0100

    done with chapter 6

--- src/script.js
+++ src/script.js
@@ -1,60 +1,67 @@
 import * as THREE from 'three';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-// Objects
-const group = new THREE.Group()
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({
-        color: 0xff0000
-    })
-)
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({
-        color: 0x00ff00
-    })
-)
-cube2.position.x = -2
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({
-        color: 'yellow'
-    })
-)
-cube3.position.x = 2
-group.add(cube3)
-
-// Axes Helper
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper)
-
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xcd2546 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set(0, 0, 3)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
-camera.lookAt(group.position)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
 
+// Clock
+// const clock = new THREE.Clock();
+
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.to(mesh.position, {
+    x: 2,
+    duration: 1,
+    delay: 1,
+    
+})
+
+
+// Animations
+const tick = () => {
+
+
+
+    // const elapsedTime = clock.getElapsedTime();
+
+    // Update objects
+    // camera.position.y = Math.cos(elapsedTime); // Rotate object around x axis.
+    // camera.position.x = Math.sin(elapsedTime); // Rotate object around x axis.
+    // camera.lookAt(mesh.position)
+
+    // Render
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
