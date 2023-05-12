4ccb6019aad42f73254e580b2d22dd6aab403eb8
Folarin Lawal <lawalfolarin6@gmail.com>
2023-05-12 17:12:20 +0100

    done with sections

--- src/script.js
+++ src/script.js
@@ -1,5 +1,52 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+import { gsap } from 'gsap';
+
+// Textures
+const loadingManager = new THREE.LoadingManager();
+const textureLoader = new THREE.TextureLoader(loadingManager);
+// const texture = textureLoader.load('/textures/scifi_metal/normal.jpg');
+
+const colorTexture = textureLoader.load('/textures/scifi_metal/base.jpg');
+const alphaTexture = textureLoader.load('/textures/scifi_metal/opacity.jpg');
+const heightTexture = textureLoader.load('/textures/scifi_metal/height.png');
+const normalTexture = textureLoader.load('/textures/scifi_metal/normal.jpg');
+const ambientOcclusionTexture = textureLoader.load(
+  '/textures/scifi_metal/ambientOcclusion.jpg'
+);
+const metalnessTexture = textureLoader.load(
+  '/textures/scifi_metal/metallic.jpg'
+);
+const roughnessTexture = textureLoader.load(
+  '/textures/scifi_metal/roughness.jpg'
+);
+
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping;
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping;
+
+// colorTexture.center.y = 0.5
+// colorTexture.center.x = 0.5
+
+// colorTexture.rotation = Math.PI / 4
+// colorTexture.generateMipmaps = false
+// colorTexture.minFilter = THREE.NearestFilter;
+// colorTexture.magFilter = THREE.NearestFilter;
+
+loadingManager.onLoad = () => {
+  gsap.to('.loader', {
+    autoAlpha: 0,
+    duration: 1,
+    delay: 1,
+  });
+};
+
+/**
+ *
+ * Debug
+ *
+ **/
+const gui = new GUI();
 
 /**
  * Base
@@ -10,14 +57,34 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
+// Scene Settings
+scene.background = new THREE.Color('#000008');
+
 /**
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ map: colorTexture });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Debug
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('boxElevation');
+
+gui.add(colorTexture.repeat, 'x')
+  .min(1)
+  .max(6)
+  .step(1)
+  .name('textureRepeatX')
+
+gui.add(colorTexture.repeat, 'y')
+  .min(1)
+  .max(6)
+  .step(1)
+  .name('textureRepeatY')
+
+gui.add(material, 'wireframe')
+  .name('enableWireframe')
 /**
  * Sizes
  */
