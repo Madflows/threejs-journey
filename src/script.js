f28984473009c8f903f1d88d4e1c55c541144750
Folarin Lawal <lawalfolarin6@gmail.com>
2023-05-24 15:41:17 +0100

    done with particle class

--- src/script.js
+++ src/script.js
@@ -1,6 +1,16 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'lil-gui';
+import { gsap } from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+
+// Register plugins
+gsap.registerPlugin(SplitText);
+
+// Hide Loader for now
+gsap.set('.loader', {
+  autoAlpha: 0,
+});
 
 /**
  * Base
@@ -14,11 +24,16 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
+
 /**
  * Textures
  */
 const loadingManager = new THREE.LoadingManager();
 const textureLoader = new THREE.TextureLoader(loadingManager);
+
+// Load Textures
+const particleTexture = textureLoader.load('/textures/particles/9.png');
+const planetTexture = textureLoader.load('/textures/2k_moon.jpg');
 
 // Animate loader on load
 loadingManager.onLoad = () => {
@@ -41,15 +56,53 @@ loadingManager.onLoad = () => {
     });
 };
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial()
-);
-scene.add(cube);
+/* Particles */
+// Geometry
+const particleGeometry = new THREE.BufferGeometry();
+const count = 10000;
 
+const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 15;
+  colors[i] = Math.random();
+}
+
+particleGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positions, 3)
+);
+particleGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial();
+particlesMaterial.size = 0.07;
+particlesMaterial.sizeAttenuation = true;
+particlesMaterial.color = new THREE.Color(0xffffff);
+particlesMaterial.transparent = true;
+particlesMaterial.alphaMap = particleTexture;
+// particlesMaterial.alphaTest = 0.001;
+// particlesMaterial.depthTest = false;
+particlesMaterial.depthWrite = false;
+particlesMaterial.blending = THREE.AdditiveBlending;
+// particlesMaterial.vertexColors = true;
+
+gui.addColor(particlesMaterial, 'color').name('particleColor');
+
+// points
+const particles = new THREE.Points(particleGeometry, particlesMaterial);
+scene.add(particles);
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  new THREE.MeshBasicMaterial({
+    wireframe: true,
+    map: planetTexture,
+  })
+);
+
+scene.add(sphere);
 /**
  * Sizes
  */
@@ -88,6 +141,7 @@ scene.add(camera);
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+controls.enableZoom = false;
 
 /**
  * Renderer
@@ -106,6 +160,20 @@ const clock = new THREE.Clock();
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
+  // Animate particles
+  particles.rotation.y = elapsedTime * 0.02;
+
+  // for (let i = 0; i < count; i++) {
+  //   const i3 = i * 3;
+  //   const x = particleGeometry.attributes.position.array[i3 + 0];
+  //   particleGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+  // }
+  // particleGeometry.attributes.position.needsUpdate = true
+  // animate planet
+  sphere.rotation.y = -elapsedTime * 0.1;
+  sphere.rotation.x = -elapsedTime * 0.1;
+
+
   // Update controls
   controls.update();
 
@@ -117,3 +185,38 @@ const tick = () => {
 };
 
 tick();
+
+// Timelines
+let rotateTL = gsap.timeline({
+  paused: true,
+});
+const splitText = new SplitText('.drag-helper span', {
+  type: 'chars',
+});
+let chars = splitText.chars;
+rotateTL
+  .to('.drag-helper i', {
+    rotate: '90deg',
+  })
+  .to(
+    chars,
+    {
+      yPercent: 100,
+      autoAlpha: 0,
+      stagger: 0.05,
+    },
+    '<'
+  )
+  .to(
+    '.drag-helper',
+    {
+      autoAlpha: 0,
+    },
+    '<'
+  );
+window.document.body.addEventListener('mousedown', () => {
+  rotateTL.play();
+});
+window.document.body.addEventListener('mouseup', () => {
+  rotateTL.reverse();
+});
